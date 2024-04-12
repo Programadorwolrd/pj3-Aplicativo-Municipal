56e9ed670512bb9a4c4422fa480fcb4001674b35
Flavio <l.flavio.@aluno.ifsp.edu.br>
2024-04-12 17:52:45 -0300

    teste

--- src/app/(app)/(profile)/_layout.tsx
+++ src/app/(app)/(profile)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
 
 export default function ProfileLayout() {
-  return <Stack />;
+  return <Stack />    
 }
+
--- src/app/(app)/(profile)/index.tsx
+++ src/app/(app)/(profile)/index.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { FlatList } from 'react-native';
+import { Text, View, Card} from 'tamagui';
 
-export default function Profile() {
+
+interface User {
+  id: number;
+  nome: string;
+  nascimento: Date;
+  email: string;
+}
+
+const usuarios: User[] = [
+  {
+    id: 1,
+    nome: 'João Silva',
+    nascimento: new Date(),
+    email: 'joao.silva@example.com',
+  },
+  {
+    id: 2,
+    nome: 'Maria Oliveira',
+    nascimento: new Date(),
+    email: 'maria.oliveira@example.com',
+  },
+];
+
+function Itens({ item: {email,id,nascimento,nome} }: { item: User }) {
+  return (
+    <View >
+
+      <Text>{email}</Text>
+      <Text>{id}</Text>
+      <Text>{nascimento.toLocaleString()}</Text>
+      <Text>{nome}</Text>
+    </View>
+  );
+}
+
+export default function Paia() {
   return (
     <View>
-      <Text>profile</Text>
+      <FlatList data={usuarios} renderItem={Itens} />
     </View>
   );
 }
