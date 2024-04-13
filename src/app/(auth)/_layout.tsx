5b01dd5e0947c49ed9853c1718507ffd9df04654
leonardo <leonardolfelix12@gmail.com>
2024-04-13 12:20:55 -0300

    feat(auth): add safeArea e imagem de background  no layout

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,9 +1,23 @@
 import { Stack } from 'expo-router';
+import { ImageBackground } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styled } from 'tamagui';
 
 export default function AuthLayout() {
+  const image = require('@/assets/background-login.png');
+
   return (
-    <Stack>
-      <Stack.Screen name='index' />
-    </Stack>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={image} resizeMode='cover' style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: 'transparent' },
+          }}
+        >
+          <Stack.Screen name='index' />
+        </Stack>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
