59ac0e17a6dfdefd424778dd56ca7bb62206d677
leonardo <leonardolfelix12@gmail.com>
2024-04-13 10:31:40 -0300

    feat(providerAuth): criando Store

--- src/providers/providerAuth.tsx
+++ src/providers/providerAuth.tsx
@@ -1,13 +1,38 @@
 import { View, Text } from 'react-native';
 import { useEffect, type PropsWithChildren, type ReactNode } from 'react';
-import { router } from 'expo-router';
+import { Redirect, router } from 'expo-router';
+import { create } from 'zustand';
+
+interface AuthState {
+  token: string | null;
+  entrar: (token: string) => void;
+  sair: () => void;
+}
+
+const userStoreAuth = create<AuthState>((set) => ({
+  token: null,
+
+  entrar: (tokenNovo: string) => {
+    set(({ token: tokenAtual }) => {
+      if (tokenAtual) throw new Error('Você já está logado');
+
+      return { token: tokenNovo };
+    });
+  },
+  sair: () => {
+    set({ token: null });
+  },
+}));
 
 export default function ProviderAuth({ children }: PropsWithChildren): ReactNode {
-  const logado = true;
+  const { token } = userStoreAuth();
 
-  useEffect(() => {
-    if (!logado) router.replace('/(auth)');
-  }, []);
+  if (!token) {
+    useEffect(() => {
+      router.replace('/(auth)/');
+    }, [token]);
+  }
 
+  
   return children;
 }
