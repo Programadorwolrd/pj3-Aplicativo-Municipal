7a3975d07e671bb7ca82082684bacf707c7ffb8c
Rayllow <franciscoryan012@gmail.com>
2024-04-15 01:56:56 -0300

    Update _layout.tsx (partial)

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,7 +1,12 @@
 import IconsTabs from '@/components/iconsTabs';
 import { Tabs } from 'expo-router';
 
-const capivaraTriste = require('@/assets/iconLay.jpeg');
+//const capivaraTriste = require('@/assets/iconLay.jpeg');
+const homeImg = require('@/assets/botao-home(1).png');
+const mapaImg = require('@/assets/mapa(1).png');
+const qrCodeImg = require('@/assets/qrCode.png');
+const profileImg = require('@/assets/avatarperfil(1).png');
+const rankingImg = require('@/assets/ranking(1).png');
 
 export default function HomeLayout() {
   return (
@@ -13,11 +18,15 @@ export default function HomeLayout() {
     >
 
      
-      <Tabs.Screen name='(home)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(mapa)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(qrCode)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(profile)' options={{  tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(ranking)' options={{  tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
+      <Tabs.Screen name='(home)' options={{ tabBarIcon: () => <IconsTabs foto={homeImg} />,
+                tabBarLabelStyle: { color: '#329F60' }, }} />
+      <Tabs.Screen name='(mapa)' options={{ tabBarIcon: () => <IconsTabs foto={mapaImg} />,
+                tabBarLabelStyle: { color: '#329F60' },  }} />
+      <Tabs.Screen name='(qrCode)' options={{ tabBarIcon: () => <IconsTabs foto={qrCodeImg} />,
+                tabBarLabelStyle: { color: '#329F60' },  }} />
+      <Tabs.Screen name='(ranking)' options={{  tabBarIcon: () => <IconsTabs foto={rankingImg} />,
+                tabBarLabelStyle: { color: '#329F60' }, }} />
+      <Tabs.Screen name='(profile)' options={{  tabBarIcon: () => <IconsTabs foto={profileImg} />  }} />
     </Tabs>
   );
 }
