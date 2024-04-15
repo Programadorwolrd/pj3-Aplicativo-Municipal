c4ee277435764ec124d73548cc7f34540f3cb979
leonardo <leonardolfelix12@gmail.com>
2024-04-15 00:48:59 -0300

    refactor: removido Providers

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,22 +1,41 @@
 import IconsTabs from '@/components/iconsTabs';
-import { Tabs } from 'expo-router';
+import { Redirect, router, Tabs, useFocusEffect } from 'expo-router';
+import { useEffect } from 'react';
 
 const capivaraTriste = require('@/assets/iconLay.jpeg');
 
 export default function HomeLayout() {
+  // simular loggin até a logica estiver completa
+  const isLogged = true;
+
+  if (!isLogged) return <Redirect href={'/(auth)/'} />;
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-
       }}
     >
-
-      <Tabs.Screen name='(mapa)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(home)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(qrCode)' options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(profile)' options={{  tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
-      <Tabs.Screen name='(ranking)' options={{  tabBarIcon: () => <IconsTabs foto={capivaraTriste} />, }} />
+      <Tabs.Screen
+        name='(mapa)'
+        options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} /> }}
+      />
+      <Tabs.Screen
+        name='(home)'
+        options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} /> }}
+      />
+      <Tabs.Screen
+        name='(qrCode)'
+        options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} /> }}
+      />
+      <Tabs.Screen
+        name='(profile)'
+        options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} /> }}
+      />
+      <Tabs.Screen
+        name='(ranking)'
+        options={{ tabBarIcon: () => <IconsTabs foto={capivaraTriste} /> }}
+      />
     </Tabs>
   );
 }
