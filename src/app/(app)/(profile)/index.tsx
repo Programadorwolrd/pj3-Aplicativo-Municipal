f0b3e674cc90a767a4ecd125b6ee9ada520bca2e
leonardo <leonardolfelix12@gmail.com>
2024-04-20 10:32:05 -0300

    fix(profile): ButtonAuth renomeado corretamente

--- src/app/(app)/(profile)/index.tsx
+++ src/app/(app)/(profile)/index.tsx
@@ -1,24 +1,25 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { Button } from 'tamagui'//importa tamagui
-import { Activity, Airplay } from '@tamagui/lucide-icons'//importa tamagui icons
-import ButtonAuth from '@/components/ButtonAuth';
-import TextAuth from '@/components/TextAuth';
+import { Button } from 'tamagui'; //importa tamagui
+import { Activity, Airplay } from '@tamagui/lucide-icons'; //importa tamagui icons
+
 import { router } from 'expo-router';
 import { XStack, YStack } from 'tamagui';
-
-
+import { ButtonCustom } from '@/components/buttonCustom';
 
 export default function Profile() {
   return (
     <View>
-       <YStack fullscreen>
-      <ButtonAuth backgroundColor="green" color="black"  bgOpaco onPress={() => router.navigate('/(auth)/preregistration')}>
-        Paia NOVO
-      </ButtonAuth>
-    </YStack> 
+      <YStack fullscreen>
+        <ButtonCustom
+          backgroundColor='green'
+          color='black'
+          bgOpaco
+          onPress={() => router.replace('/(app)/(qrCode)')}
+        >
+          Paia NOVO
+        </ButtonCustom>
+      </YStack>
     </View>
   );
 }
-
-
