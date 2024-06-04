682634a6c80538ae9db043b16d947a0440c84574
philype <lellis.sena@aluno.ifsp.edu.br>
2024-06-04 17:56:12 -0300

    meu comit sobre o home, Pagina do Philype terminada

--- src/app/(app)/(home)/_layout.tsx
+++ src/app/(app)/(home)/_layout.tsx
@@ -1,10 +1,26 @@
 import StackApp from '@/components/StackApp';
 import { Stack } from 'expo-router';
 
+import { Image, Text } from 'tamagui';
+
 export const unstable_settings = {
   initialRouteName: 'index',
 };
 
 export default function HomeLayout() {
-  return <StackApp titulo='src/assets/BioDex.png' />;
+  return <Stack screenOptions={{
+    headerTitleAlign: 'center',
+    headerTitle({ children }) {
+      return (
+        <Text display='flex' justifyContent='center' alignContent='center'>
+          <Image style={{
+            alignItems: 'center',
+            justifyContent: 'center'
+          }} source={require('../../../assets/BioDex.png')} />
+          <Image />
+        </Text>
+      );
+    },
+  }} />;
+
 }
