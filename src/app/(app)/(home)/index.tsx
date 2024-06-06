e89acbb5850c8d0affd5db4989977b0372d39484
philype <lellis.sena@aluno.ifsp.edu.br>
2024-06-06 17:42:40 -0300

    map page perfect 95%

--- src/app/(app)/(home)/index.tsx
+++ src/app/(app)/(home)/index.tsx
@@ -1,8 +1,8 @@
 import { Link } from 'expo-router';
-import { View, Text } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import mapaPage from './mapa';
 import homePage from './home';
+import { View } from 'tamagui';
 
 
 const Tab = createMaterialTopTabNavigator();
@@ -10,15 +10,16 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function Index() {
   return (
-    <View>
-    <Tab.Navigator screenOptions={{
-      tabBarLabelStyle: { fontSize: 12 },
-      tabBarActiveTintColor: '#329F60',
-      tabBarIndicatorStyle: { backgroundColor: '#329F60' }
-    }}>
-      <Tab.Screen  name="home" component={homePage} />
-      <Tab.Screen name="Mapa" component={mapaPage} />
-    </Tab.Navigator>
+    <View f={1} >
+      <Tab.Navigator screenOptions={{
+        tabBarLabelStyle: { fontSize: 12 },
+        tabBarActiveTintColor: '#329F60',
+        tabBarIndicatorStyle: { backgroundColor: '#329F60' }
+      }}>
+          <Tab.Screen name="Mapa" component={mapaPage} />
+        <Tab.Screen name="home" component={homePage} />
+     
+      </Tab.Navigator>
     </View>
   );
 }
