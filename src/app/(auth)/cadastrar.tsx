cee9ba049080c424aa24f82dd884311780475392
leonardo <leonardolfelix12@gmail.com>
2024-06-12 10:07:34 -0300

    refactor: estrutura do FormClass refeita

--- src/app/(auth)/cadastrar.tsx
+++ src/app/(auth)/cadastrar.tsx
@@ -1,45 +1,49 @@
-import { FormAuth } from '@/components/FormClass';
-import FormFooter from '@/components/FormFooter';
-import TAuth from '@/components/templateAuth';
-import { allvalids } from '@/lib/allValids';
-import { storeAuth } from '@/lib/logicAuth';
-import { router } from 'expo-router';
+import { FormPaia } from "@/components/FormClass";
+import { TemplateInput } from "@/components/FormDefault";
+import FormFooter from "@/components/FormFooter";
+import TAuth from "@/components/templateAuth";
+import { allvalids } from "@/lib/allValids";
+import { storeAuth } from "@/lib/logicAuth";
+import { router } from "expo-router";
+import { Input } from "tamagui";
 
 export default function Cadastrar() {
   const login = storeAuth((s) => s.login);
 
-  const SignUp = new FormAuth({
-    campos: allvalids,
-    onSubmit: (axios) => ({
-      mutationFn: (allValues) => axios.post('/usuario', allValues),
-      notlogoutIfNotAuthorized: true,
-      async onSuccess(_, allValues) {
-        const { apelido, ...credentials } = allValues;
+  const SignUp = new FormPaia(
+    {
+      campos: allvalids,
+      submitOptions: (axios) => ({
+        mutationFn: (allValues) => axios.post("/usuario", allValues),
+        notlogoutIfNotAuthorized: true,
+        async onSuccess(_, allValues) {
+          const { apelido, ...credentials } = allValues;
 
-        const { data } = await axios.post('/usuario/login', credentials);
+          const { data } = await axios.post("/usuario/login", credentials);
 
-        login(data.token);
+          login(data.token);
 
-        router.replace('/(app)/(home)');
-      },
-    }),
-  });
+          router.replace("/(app)/(home)");
+        },
+      }),
+    },
+    Input,
+    TemplateInput
+  );
 
   return (
-    <TAuth subTitulo='Cadastra-se ' titulo='CADASTRAR'>
-      <SignUp.Form>
-        <SignUp.Input campo='apelido' />
-        <SignUp.Input campo='email' textContentType='emailAddress' />
-        <SignUp.Input campo='senha' secureTextEntry textContentType='password' />
-        <SignUp.Submit textButton='CADASTRAR' />
-        <FormFooter
-          link={{
-            href: '/(auth)/login',
-            text: 'Já está cadastrado?',
-            textLink: 'Entre aqui!',
-          }}
-        />
-      </SignUp.Form>
+    <TAuth subTitulo="Cadastra-se " titulo="CADASTRAR">
+      <SignUp.Input campo="" />
+      <SignUp.Input campo="email" textContentType="emailAddress" />
+      <SignUp.Input campo="senha" secureTextEntry textContentType="password" />
+      <SignUp.Submit textButton="CADASTRAR" />
+      <FormFooter
+        link={{
+          href: "/(auth)/login",
+          text: "Já está cadastrado?",
+          textLink: "Entre aqui!",
+        }}
+      />
     </TAuth>
   );
 }
--- src/components/FormClass.tsx
+++ src/components/FormClass.tsx
@@ -1,187 +1,135 @@
-import {
-  ComponentClass,
-  FC,
-  PropsWithChildren,
-  ReactNode,
-  useEffect,
-  useReducer,
-  useRef,
-  useState,
-} from "react";
-
-import type { AxiosInstance } from "axios";
+import { FC, PropsWithChildren, useEffect, useRef, useState } from "react";
 import type {
   UseMutationOptions,
   UseMutationResult,
 } from "@tanstack/react-query";
+import useApi, { CallbackAxios } from "@/lib/useApi";
+import { TextInputProps } from "react-native";
 
-import useApi from "@/lib/useApi";
-import { InputProps } from "tamagui";
-import { TextInput, TextInputProps } from "react-native";
+export class FormPaia<C extends string, Inp extends InputBase> {
+  private values = useRef({} as ObjC<C, string>).current;
+  private useApiPaia: UseMutation<typeof this.values>["result"];
+
+  constructor(
+    private options: Options<C>,
+    private InputType: FC<Inp>,
+    private TemplateInput: FC<TemplateInputProps>
+  ) {
+    this.useApiPaia = useApi("mutate", options.submitOptions);
+  }
+
+  /*
+   * função para submeter o formulário
+   */
+  public submit = () => this.useApiPaia.mutate(this.values);
+
+  /*
+   * componente que rerenderiza o que estiver dentro com as informações da tentativa de submissão
+   */
+  public Escutar = ({ children: C }: EscutarPropsPaia<C>) => (
+    <C mutation={this.useApiPaia} />
+  );
+
+  /*
+   * Componente do Input com toda lógica de validação
+   */
+  public Input = ({ value, campo, ...props }: Inp & { campo: C }) => {
+    const [valueInput, setValuesInput] = useState(this.values[campo]);
+    const infoValidy = this.useValid(value || "", campo);
+
+    this.values[campo] = valueInput;
+
+    return (
+      <this.TemplateInput campo={campo} {...infoValidy}>
+        <this.InputType
+          {...(props as unknown as Inp)}
+          value={value}
+          onChangeText={(v) => setValuesInput(v)}
+        />
+      </this.TemplateInput>
+    );
+  };
+
+  /**
+   *  Hook que cuida de validar os valores do input
+   */
+  private useValid(value: string, campo: C) {
+    const [aviso, setAviso] = useState<StateValues<typeof State>>(State.EMPTY);
+
+    useEffect(() => {
+      setAviso(State.LOADING);
+
+      const timeoutId = setTimeout(async () => {
+        const validadoresDoCampo = this.options.campos?.[campo];
+        if (!validadoresDoCampo) return setAviso(State.EMPTY); // não existe validacao para esse campo
+
+        for await (const [verify, avisoDoErro] of validadoresDoCampo) {
+          if (await verify(value)) {
+            return setAviso(avisoDoErro); // ERROR
+          }
+        }
+
+        // se passou nos testes seta null como sucesso
+        setAviso(State.SUCCESS);
+      }, 2000);
+
+      return () => clearTimeout(timeoutId);
+    }, [value]);
+
+    return {
+      aviso,
+      isLoading: aviso == State.LOADING,
+      isError: aviso instanceof String,
+      isValid: aviso === State.SUCCESS,
+    };
+  }
+}
+
+// 
+
+// AVISO: tipagem abaixo
+
+//
+
+export type TemplateInputProps = PropsWithChildren<{
+  campo: string;
+  aviso: StateValues<typeof State>;
+  isLoading: boolean;
+  isError: boolean;
+  isValid: boolean;
+}>;
+
+const State = {
+  LOADING: false,
+  SUCCESS: true,
+  EMPTY: null,
+  ERROR: new String(), // qualquer string é considerado erro
+} as const;
+
+type StateValues<O> = {
+  [K in keyof O]: O[K];
+}[keyof O];
+
+type ValidacoesCampo =
+  | [(t: string) => boolean | Promise<boolean>, string][]
+  | null;
+
+type ObjC<C extends string, Value> = {
+  [K in C]: Value;
+};
+
+type Options<C extends string> = {
+  campos: ObjC<C, ValidacoesCampo>;
+  submitOptions: CallbackAxios<UseMutation<ObjC<C, string>>["options"]>;
+};
 
 type UseMutation<objC> = {
   options: UseMutationOptions<unknown, Error, objC>;
   result: UseMutationResult<unknown, Error, objC>;
 };
 
-type InputPropsPaia<P> = P extends InputProps ? InputProps : TextInputProps;
-
-type Options<C extends string, In> = {
-  campos: {
-    [key in C]: [];
-  };
-  InputComponent: ComponentClass<InputPropsPaia<In>> & In;
-  submitOptions: (
-    axios: AxiosInstance
-  ) => UseMutation<{ [key in C]: string }>["options"];
-};
-type Values<C extends string> = {
-  [key in C]: string;
-};
 type EscutarPropsPaia<C extends string> = {
-  children: FC<{ mutation: UseMutation<Values<C>>["result"] }>;
+  children: FC<{ mutation: UseMutation<ObjC<C, string>>["result"] }>;
 };
 
-// refatorando a classe para n depender do tamagui
-export class FormPaia<C extends string, In> {
-  private values = useRef({} as Values<C>).current;
-  private useApiPaia: UseMutation<typeof this.values>["result"];
-
-  constructor(private options: Options<C, In>) {
-    this.useApiPaia = useApi("mutate", options["submitOptions"]);
-  }
-
-  public Input = ({
-    value,
-    campo,
-    ...props
-  }: InputPropsPaia<In> & { campo: C }) => {
-    const [valueInput, setValuesInput] = useState(this.values[campo] || "");
-
-    const InputPaiado = this.options.InputComponent as FC<typeof props>;
-    return <InputPaiado {...props} />;
-  };
-
-  public Escutar = ({ children: Children }: EscutarPropsPaia<C>) => (
-    <Children mutation={this.useApiPaia} />
-  );
-
-  public submit = () => this.useApiPaia.mutate(this.values);
-}
-
-// class FormAuthAntigo<C extends ValidacaoOptions> {
-//   private AllValuesRef = useRef({} as Record<keyof C, string>).current;
-//   private isFormValid = useRef({} as Record<string, boolean>).current;
-//   private useApiForm!: UseMutationResult;
-
-//   constructor(private options: FormOptions<C>) {
-//     this.useApiForm = useApi("mutate", options.onSubmit) as UseMutationResult;
-//   }
-
-//   private AvisoEValidador = (props: { campo: string; value: string }) => {
-//     const [aviso, setAviso] = useState("");
-
-//     const arrayDeValidacao = this.options.campos[props.campo];
-
-//     useEffect(() => {
-//       if (arrayDeValidacao == null) return;
-
-//       const ValidarCampo = async () => {
-//         try {
-//           for await (const [condi, avisoDaCondi] of arrayDeValidacao) {
-//             if (await condi(props.value)) throw avisoDaCondi; //se atender a condição do erro ele lança uma execao com o texto do aviso
-
-//             this.isFormValid[props.campo] = true;
-//             setAviso("");
-//           }
-//         } catch (error) {
-//           if (typeof error == "string") {
-//             this.isFormValid[props.campo] = false;
-//             setAviso(error);
-//           }
-//         }
-//       };
-
-//       ValidarCampo();
-//     }, [props.value]);
-
-//     const color = aviso ? "red" : "green";
-//     return (
-//       <Text fontSize={"$4"} fontFamily={"$paiaNormal"} color={color}>
-//         {aviso || `${props.campo} valido`}
-//       </Text>
-//     );
-//   };
-
-//   private onSubmit = () => {
-//     const isNotValid = Object.values(this.isFormValid).includes(false);
-
-//     if (isNotValid) {
-//       Platform.OS === "web"
-//         ? alert("formulario invalido")
-//         : Alert.alert("formulario invalido", "finalize o formulario");
-
-//       return;
-//     }
-
-//     this.useApiForm.mutate(this.AllValuesRef);
-//   };
-
-//   public Submit = (props: { textButton: string }) => {
-//     const { isPending } = this.useApiForm;
-
-//     return (
-//       <FormTamagui.Trigger asChild disabled={isPending}>
-//         <ButtonCustom disabled={isPending} marginLeft="-2%" width="104%">
-//           {isPending ? "aguarde" : props.textButton}
-//           {isPending && <Spinner size="large" color={"$blue10Dark"} />}
-//         </ButtonCustom>
-//       </FormTamagui.Trigger>
-//     );
-//   };
-
-//   public Input = ({
-//     campo,
-//     ...props
-//   }: InputProps & { campo: keyof C & string }) => {
-//     const [value, setValue] = useState(this.AllValuesRef[campo] || "");
-//     this.AllValuesRef[campo] = value;
-
-//     return (
-//       <YStack w="100%">
-//         <Text
-//           fontSize={"$2"}
-//           color="green"
-//           mb="$1.5"
-//           fontFamily={"$outfitBold"}
-//         >
-//           {campo.toUpperCase()}:
-//         </Text>
-//         <InputStyled
-//           value={value}
-//           onChangeText={setValue}
-//           {...props}
-//           placeholder={campo}
-//         />
-//         <this.AvisoEValidador campo={campo} value={value} />
-//       </YStack>
-//     );
-//   };
-
-//   public Form = ({ children, ...props }: Omit<FormProps, "onSubmit">) => (
-//     <FormTamagui
-//       w={"100%"}
-//       alignItems="center"
-//       {...props}
-//       onSubmit={this.onSubmit}
-//     >
-//       <YStack width={"100%"} gap={"$4"}>
-//         {children}
-//       </YStack>
-//     </FormTamagui>
-//   );
-// }
-
-type ValidacoesCampo =
-  | [(t: string) => boolean | Promise<boolean>, string][]
-  | null;
+type InputBase = Pick<TextInputProps, "value" | "onChangeText">;
