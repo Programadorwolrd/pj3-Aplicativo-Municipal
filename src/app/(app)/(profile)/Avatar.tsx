54cf2567969532fee406140ec36651e6e72fe927
Guxtavo9 <pereira.gustavo2@aluno.ifsp.edu.br>
2024-06-14 16:44:25 -0300

    feat: logout btn e avatar modal

--- src/app/(app)/(profile)/Avatar.tsx
+++ src/app/(app)/(profile)/Avatar.tsx
@@ -1,30 +1,101 @@
 import { Avatar, YStack, Text } from "tamagui";
-import { CardDemo } from "./Test";
+import React, { useState } from "react";
+import { Alert, Modal, Pressable, StyleSheet, Image } from "react-native";
 // const avatarPerfil = require("../../../assets/avatar-icon.jpeg");
-
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { X } from "@tamagui/lucide-icons";
 interface avatarPerfil {
   img: string;
 }
 
+
+
 export default function AvatarProfile({ img }: avatarPerfil) {
+  const [modalVisible, setModalVisible] = useState(false);
   //teste
   const avatar: avatarPerfil = {
     img
   };
 
   if (!avatar.img) {
-    // avatar.img = "https://pbs.twimg.com/profile_images/1464251436284485632/8tFZcXcg_400x400.jpg";
     avatar.img = "https://t3.ftcdn.net/jpg/03/58/90/78/360_F_358907879_Vdu96gF4XVhjCZxN2kCG0THTsSQi8IhT.jpg";
   }
 
   return (
     <YStack mt={"$3"} ai={"center"}>
-      <Avatar circular size={"$11"}>
-        <Avatar.Image accessibilityLabel="avatar" src={avatar.img} />
-        <Avatar.Fallback backgroundColor="$blue10" />
-      </Avatar>
-      {/* <CardDemo /> */}
-      {/* <Text mt={"$3"}>alterar incone</Text> */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+          {/* <SafeAreaView style={{flex: 1}} onTouch={() => setModalVisible(!modalVisible)}> */}
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+          <Avatar circular size={"$20"}>
+          <Avatar.Image accessibilityLabel="avatar" src={avatar.img} />
+          <Avatar.Fallback backgroundColor="$blue10" />
+        </Avatar>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <X size={30} color={"#000"} />
+              {/* <Text style={styles.textStyle}>Hide Modal</Text> */}
+            </Pressable>
+          </View>
+        </View>
+        {/* </SafeAreaView> */}
+      </Modal>
+
+      <Pressable onPress={() => setModalVisible(true)}>
+        <Avatar circular size={"$11"}>
+          <Avatar.Image accessibilityLabel="avatar" src={avatar.img} />
+          <Avatar.Fallback backgroundColor="$blue10" />
+        </Avatar>
+        {/* <Text mt={"$3"}>alterar incone</Text> */}
+      </Pressable>
     </YStack>
   );
 }
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+
+    borderRadius: 20,
+    alignItems: 'center',
+  
+  },
+  tinyLogo: {
+    width: 250,
+    height: 250,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: 'transparent',
+  },
+  buttonClose: {
+    backgroundColor: 'transparent',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
--- src/app/(app)/(profile)/index.tsx
+++ src/app/(app)/(profile)/index.tsx
@@ -1,71 +1,74 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Modal, Pressable } from "react-native";
 import { router } from "expo-router";
 import { ButtonCustom } from "@/components/buttonCustom";
 import { storeAuth } from "@/lib/logicAuth";
 import useApi from "@/lib/useApi";
-import { YStack, Image, Text, XStack } from "tamagui";
+import { YStack, Image, Text, XStack, View } from "tamagui";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AvatarProfile from "./Avatar";
 import ProfileData from "./ProfileData";
 import Tabs from "./(tabs)";
+import { Button } from 'react-native-elements';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import axios from "axios";
 
 const backProfile = require("../../../assets/background-perfil.png");
 
 interface PropsUser {
+  id: string;
   apelido: string;
-  foto: string,
-  lidopelouser: {
-    catalogo: {
-      uuid: string,
-      nomePopular: string
-    }
-  },
+  foto: string;
+  // lidopelouser: {
+  //   catalogo: {
+  //     uuid: string,
+  //     nomePopular: string
+  //   }
+  // },
   ranking: number;
 }
-
 
 export default function Profile() {
   // botão de logout e delete de conta
 
-  // const loggout = storeAuth((s) => s.logout);
+  const loggout = storeAuth((s) => s.logout);
 
-  // const { data, refetch } = useApi("query", (axios) => ({
-  //   queryKey: ["getMyProfile"],
-  //   queryFn() {
-  //     return axios.get("/usuario");
-  //   },
-  // }));
+  const { data, refetch } = useApi("query", (axios) => ({
+    queryKey: ["getMyProfile"],
+    queryFn() {
+      return axios.get("/usuario");
+    },
+  }));
 
-  // const { mutate } = useApi("mutate", (axios) => ({
-  //   mutationFn() {
-  //     return axios.delete("/usuario");
-  //   },
-  // }));
+  const { mutate } = useApi("mutate", (axios) => ({
+    mutationFn() {
+      return axios.delete("/usuario");
+    },
+  }));
+
   const user = useApi("query", (axios) => {
     return {
-      queryKey: ['xabulha'],
+      queryKey: ["user"],
       queryFn: () => {
-        return axios.get('/usuario')
-
-      }
-    }
-  })
-  console.log(user.data?.data.usuario, 'user');
+        return axios.get("/usuario");
+      },
+    };
+  });
+  console.log(user.data?.data.usuario, "user");
 
   const dataUser: PropsUser = {
-    apelido: user.data?.data.usuario.apelido,
-    foto: user.data?.data.usuario.foto,
-    lidopelouser: {
-      catalogo: {
-        uuid: user.data?.data?.usuario?.catalogo?.uuid,
-        nomePopular: user.data?.data?.usuariocatalogo?.nomePopular
-      }
-    },
-    ranking: 3
-  }
-  console.log(dataUser, 'data user')
+    id: user.data?.data.usuario.id || "",
+    apelido: user.data?.data.usuario.apelido || "",
+    foto: user.data?.data.usuario.foto || "",
+    // lidopelouser: {
+    //   catalogo: {
+    //     uuid: user.data?.data?.usuario?.catalogo?.uuid,
+    //     nomePopular: user.data?.data?.usuariocatalogo?.nomePopular
+    //   }
+    // },
+    ranking: 3,
+  };
+  console.log(dataUser, "data user profile");
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -79,16 +82,29 @@ export default function Profile() {
             position="absolute"
             zi={"$0"}
           />
+
           <XStack mt={"$3"} ai={"center"} jc={"center"}>
             <Text
               fontSize={"$8"}
               fontWeight={"bold"}
-              mt={"$5"}
+              mt={"$3"}
               color={"$white2"}
             >
               Perfil
             </Text>
-
+            <View style={{ position: "absolute", right: 20, top: 6 }}>
+              <Button
+                onPress={loggout}
+                icon={
+                  <Ionicons
+                    name="ellipsis-vertical"
+                    size={24}
+                    color="white"
+                  />
+                }
+                type="clear" // Este tipo remove o fundo padrão do botão
+              />
+            </View>
           </XStack>
           <ProfileData nome={dataUser.apelido} ranking={dataUser.ranking} />
           <AvatarProfile img={dataUser.foto} />
@@ -96,10 +112,9 @@ export default function Profile() {
         <Tabs />
 
         {/* componente do botão de logout e delete de conta */}
-
-        
       </YStack>
     </SafeAreaView>
-
   );
 }
+
+
