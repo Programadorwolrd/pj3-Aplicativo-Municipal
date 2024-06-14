a205b057c6fbe0e2ff4190ab15a7e863cb37d91e
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2024-06-14 15:54:14 -0300

    home 95%

--- src/components/CardSeres.tsx
+++ src/components/CardSeres.tsx
@@ -8,20 +8,18 @@ export interface PropsCardSeres {
     categoria: string;
 }
 export default ({ nome, link, photo, categoria }: PropsCardSeres) =>
-    <View backgroundColor={'#329F60'} width={150} height={160} borderRadius={20} paddingBottom={100}
-        style={{
-           zIndex: 1
-        }}>
-        <View width={150} display={'flex'} justifyContent='center' alignItems='center' >
-            <Image style={{ width: 110, height: 100, marginTop: -10, zIndex: 2 }} source={require("../assets/passaro.png")} />
+    <View width={150} height={170} display='flex' justifyContent='flex-end'>
+        <View backgroundColor={'#329F60'} borderRadius={20} paddingBottom={100}>
+            <Image style={{ width: 110, height: 100, marginTop: -10 }} source={require("../assets/passaro.png")} />
+
+
+            <View marginLeft={10}>
+                <Text color={'#fff'} fontWeight='normal'>{nome}</Text>
+                <Text color={'#fff'} >{categoria}</Text>
+
+            </View>
 
         </View>
-        <View marginLeft={10}>
-            <Text color={'#fff'} fontWeight='normal'>{nome}</Text>
-            <Text color={'#fff'} >{categoria}</Text>
-
-        </View>
-
     </View>
 
 
