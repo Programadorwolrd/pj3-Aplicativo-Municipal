8e51bf390e65ca7fcad77bc199f49904f0f6dfdf
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2024-06-16 13:39:43 -0300

    ajustando layout da home

--- src/app/(app)/(home)/index.tsx
+++ src/app/(app)/(home)/index.tsx
@@ -2,13 +2,14 @@ import { Link } from "expo-router";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import mapaPage from "./mapa";
 import homePage from "./home";
-import { View } from "tamagui";
+import { ScrollView, View } from "tamagui";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function Index(a) {
   return (
     <View f={1}>
+      <ScrollView>
       <Tab.Navigator
         screenOptions={{
           tabBarLabelStyle: { fontSize: 12 },
@@ -19,6 +20,7 @@ export default function Index(a) {
         <Tab.Screen name="home" component={homePage} />
         <Tab.Screen name="Mapa" component={mapaPage} />
       </Tab.Navigator>
+      </ScrollView>
     </View>
   );
 }
