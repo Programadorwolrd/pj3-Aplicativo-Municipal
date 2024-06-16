83137d110e1f3d608dc7e20389d758352e7b022d
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-06-16 10:50:12 -0300

    add especie and change button

--- src/app/(app)/(home,qrCode)/[infoUrl].tsx
+++ src/app/(app)/(home,qrCode)/[infoUrl].tsx
@@ -2,7 +2,9 @@ import {
   FlatList,
   StyleSheet,
   Dimensions,
-  Alert,
+  TouchableWithoutFeedback,
+  Modal,
+  TouchableHighlight,
   type ImageSourcePropType,
   type NativeSyntheticEvent,
   type NativeScrollEvent,
@@ -11,7 +13,6 @@ import {
 import React, { useEffect, useRef, useState, type ReactNode } from "react";
 import { Button, Image, ScrollView, Text, View } from "tamagui";
 import { TabsDemo } from "../../../components/tabs";
-import { AirbnbRating } from "react-native-ratings";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
@@ -66,10 +67,9 @@ export default function InfoUrl() {
   interface ContentData {
     nomePopular: string;
     nomeCientifico: string;
-    nascimento: string;
     som: string;
+    especie: string;
     tabsMenu: string;
-    estrela: number;
     medalha: string;
     catalogoGaleria: Array<{
       id: number;
@@ -81,34 +81,37 @@ export default function InfoUrl() {
 
   const [carouselData, setCarouselData] = useState<CarouselData[]>([]);
   const [content, setContent] = useState<ContentData | null>(null);
-  const [notFound, setNotFound] = useState <boolean | null>(null)
+  const [notFound, setNotFound] = useState<boolean | null>(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(`/usuario/lerqrcode/${infoUrl}`);
-      
+
         if (!data || !data.catalogo) {
           setNotFound(true);
           return;
         }
-      
+
         setCarouselData(data.catalogo.catalogoGaleria);
         setContent(data.catalogo);
-      } catch (error) {        
+      } catch (error) {
         if (isAxiosError(error)) console.log(error?.response?.data);
         setNotFound(true);
       }
     };
-  
+
     fetchData();
   }, [infoUrl, navigation]);
 
-  
-  if (notFound) return <ErrorScreen
-  title= "Ser vivo não encontrado"
-  text = "Escaneie um QrCode valido"
-/>
+  if (notFound)
+    return (
+      <ErrorScreen
+        title="Ser vivo não encontrado"
+        text="Escaneie um QrCode valido"
+      />
+    );
 
   const renderItem = ({
     item,
@@ -175,121 +178,175 @@ export default function InfoUrl() {
     });
   };
 
-
- if(content) return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ position: "absolute", top: 50, left: 10, zIndex: 1 }}>
-        <Pressable onPress={() => navigation.goBack()}>
-          <Ionicons name="arrow-back" size={40} color="white" />
-        </Pressable>
-      </View>
-      <ScrollView style={{ backgroundColor: "black" }}>
-        <View style={{ position: "relative" }}>
-          <FlatList
-            data={carouselData}
-            ref={flatlistRef}
-            getItemLayout={getItemLayout}
-            renderItem={({ item }) => (
-              <Image
-                source={{ uri: getFiles(item.url) }}
-                w={screenWidth}
-                aspectRatio={"12/15"}
-              />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-            horizontal={true}
-            pagingEnabled={true}
-            onScroll={handleScroll}
-            showsHorizontalScrollIndicator={false}
-          />
-          <View
-            style={{
-              flexDirection: "row",
-              position: "absolute",
-              bottom: 5,
-              marginHorizontal: "40%",
-              marginBottom: 40,
-            }}
-          >
-            {renderDotIndicators()}
-          </View>
+  if (content)
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ position: "absolute", top: 50, left: 10, zIndex: 1 }}>
+          <Pressable onPress={() => navigation.goBack()}>
+            <Ionicons name="arrow-back" size={40} color="white" />
+          </Pressable>
         </View>
-
-        <ScrollView
-          style={{
-            backgroundColor: "black",
-            borderTopLeftRadius: 40,
-            overflow: "hidden",
-            marginTop: -36,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              margin: 20,
-              justifyContent: "space-between",
-              marginBottom: -15,
-            }}
-          >
-            <Text style={{ color: "white", fontSize: 18, marginVertical: 15 }}>
-              {content?.nomePopular}
-            </Text>
-            <Image
-              source={{ uri: getFiles(content?.medalha) }}
-              style={{
-                marginLeft: 10,
-                height: 50,
-                width: 50,
-                borderRadius: 50,
-              }}
+        <ScrollView style={{ backgroundColor: "black" }}>
+          <View style={{ position: "relative" }}>
+            <FlatList
+              data={carouselData}
+              ref={flatlistRef}
+              getItemLayout={getItemLayout}
+              renderItem={({ item }) => (
+                <Image
+                  source={{ uri: getFiles(item.url) }}
+                  w={screenWidth}
+                  aspectRatio={"12/15"}
+                />
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal={true}
+              pagingEnabled={true}
+              onScroll={handleScroll}
+              showsHorizontalScrollIndicator={false}
             />
+            <View
+              style={{
+                flexDirection: "row",
+                position: "absolute",
+                bottom: 5,
+                marginHorizontal: "40%",
+                marginBottom: 40,
+              }}
+            >
+              {renderDotIndicators()}
+            </View>
           </View>
 
-          <Text
+          <ScrollView
             style={{
-              color: "#878787",
-              fontSize: 18,
-              marginHorizontal: 20,
-              marginTop: 10,
-              marginBottom: 5,
+              backgroundColor: "black",
+              borderTopLeftRadius: 40,
+              overflow: "hidden",
+              marginTop: -36,
             }}
           >
-            {content?.nomeCientifico}
-          </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                margin: 20,
+                justifyContent: "space-between",
+                marginBottom: -15,
+              }}
+            >
+              <Text
+                style={{ color: "white", fontSize: 18, marginVertical: 15 }}
+              >
+                {content?.nomePopular}
+              </Text>
+              <Image
+                source={{ uri: getFiles(content?.medalha) }}
+                style={{
+                  marginLeft: 10,
+                  height: 50,
+                  width: 50,
+                  borderRadius: 50,
+                }}
+              />
+            </View>
 
-          <Text style={{ marginHorizontal: 20, marginVertical: 5 }}>
-            <AirbnbRating
-              count={5}
-              reviewSize={0}
-              defaultRating={content?.estrela}
-              selectedColor="#329F60"
-              showRating={false}
-              reviewColor="green"
-              size={20}
-              isDisabled={true}
-            />
-          </Text>
-          <TabsDemo catalogo={content} />
-          <Button
-            onPress={() =>
-              Alert.alert("Sucesso", "Você capturou o Ser vivo com sucesso!")
-            }
-            style={{
-              backgroundColor: "white",
-              color: "black",
-              fontSize: 18,
-              marginHorizontal: "auto",
-              marginVertical: 20,
-              width: 340,
-            }}
-          >
-            Capturar Ser vivo 🌿
-          </Button>
+            <Text
+              style={{
+                color: "#878787",
+                fontSize: 18,
+                marginHorizontal: 20,
+                marginTop: 10,
+                marginBottom: 5,
+              }}
+            >
+              {content?.nomeCientifico}
+            </Text>
+
+            <Text
+              style={{
+                color: "#878787",
+                fontSize: 18,
+                marginHorizontal: 20,
+                marginTop: 10,
+                marginBottom: 5,
+              }}
+            >
+              {content?.especie}
+            </Text>
+
+            <TabsDemo catalogo={content} />
+            <Button
+              onPress={() => setModalVisible(true)}
+              style={{
+                backgroundColor: "white",
+                color: "black",
+                fontSize: 18,
+                marginHorizontal: "auto",
+                marginVertical: 20,
+                width: 340,
+              }}
+            >
+              Capturar Ser vivo 🌿
+            </Button>
+
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "rgba(0,0,0,0.5)",
+                  }}
+                >
+                  <TouchableWithoutFeedback>
+                    <View
+                      style={{
+                        backgroundColor: "white",
+                        padding: 20,
+                        borderRadius: 10,
+                        alignItems: "center",
+                      }}
+                    >
+                      <Text style={{ color: "black", fontSize: 16 }}>Sucesso</Text>
+                      <Text style={{ color: "black", fontSize: 16, marginBottom: 10 }}>
+                        Você capturou o Ser vivo com sucesso!
+                      </Text>
+
+                      <TouchableHighlight
+                        onPress={() => {
+                          setModalVisible(!modalVisible);
+                        }}
+                        style={{
+                          backgroundColor: "#329F60",
+                          padding: 10,
+                          marginTop: 30,
+                          borderRadius: 10,
+                          position: "absolute",
+                          bottom: -20,
+                          width: "30%",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Text style={{ color: "white", fontSize: 18 }}>Fechar</Text>
+                      </TouchableHighlight>
+                    </View>
+                  </TouchableWithoutFeedback>
+                </View>
+              </TouchableWithoutFeedback>
+            </Modal>
+          </ScrollView>
         </ScrollView>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({});
