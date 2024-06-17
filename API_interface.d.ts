00556ce81b20cb6d927517f0525de5a0e57d3e5b
leonardo <leonardolfelix12@gmail.com>
2024-06-16 21:06:38 -0300

    chore: interfaces da api centralizado e querys tipados

---
+++ API_interface.d.ts
@@ -0,0 +1,35 @@
+// TIPAGEM DAS QUERYS
+
+interface Usuario {
+  id: string;
+  apelido: string;
+  foto: string;
+  lidoPeloUser: LidoPeloUser[];
+  catalogoNLido: Catalogo[];
+  progresso: {
+    total: number;
+    lido: number;
+  };
+}
+
+interface LidoPeloUser {
+  dataDaDescoberta: string;
+  catalogo: Catalogo;
+}
+
+interface Catalogo {
+  uuid: string;
+  nomePopular: string;
+  nomeCientifico: string;
+  especie: string;
+  ftModel: string;
+  medalha: string;
+}
+
+interface Rank {
+  id: number;
+  apelido: string;
+  foto: string;
+  qrCodeUnicosLidos: number;
+  isCurrentUser: boolean;
+}
