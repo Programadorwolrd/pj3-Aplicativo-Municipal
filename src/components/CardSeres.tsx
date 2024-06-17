07ef648f28d7c472c722aec14dd885bd284c7af1
João Beserra <joao.beserra@aluno.ifsp.edu.br>
2024-06-17 16:25:06 -0300

    home 100%

--- src/components/CardSeres.tsx
+++ src/components/CardSeres.tsx
@@ -1,35 +1,43 @@
-import { Button, Text, View } from "tamagui";
-import { Image, Pressable } from "react-native";
-import { router } from "expo-router";
 import { getFiles } from "@/lib/useAxios";
+import { Image } from "tamagui";
+import { Text, View } from "tamagui";
+import { Pressable } from "react-native";
 
 export interface PropsCardSeres {
   nome: string;
   uuid: string;
   photo: string;
   categoria: string;
+  isRead: boolean; // nova propriedade
 }
-export default ({ nome, uuid, photo, categoria }: PropsCardSeres) => (
-  <Pressable onPress={() => router.navigate(`(app)/(home)/${uuid}`)}>
-    <View style={{ height: 180 }} justifyContent="flex-end">
-      <View
-        backgroundColor={"#329F60"}
-        borderRadius={20}
-        paddingBottom={100}
-        height={140}
-      >
-        <Image
-          style={{ width: 100, height: 90, marginTop: -10 }}
-          source={{ uri: getFiles(photo) }}
-        />
 
-        <View marginLeft={10}>
-          <Text color={"#fff"} fontWeight="normal">
-            {nome}
-          </Text>
-          <Text color={"#fff"}>{categoria}</Text>
+export default ({ nome, uuid, photo, categoria, isRead }: PropsCardSeres) => {
+  const backgroundColor = isRead ? "#329F60" : "gray"; // altera a cor de fundo com base em isRead
+  const onPress = isRead ? () => router.navigate(`(app)/(home)/${uuid}`) : undefined; // desabilita a navegação se o card não foi lido
+
+  return (
+    <Pressable onPress={onPress}>
+      <View style={{ height: 180 }} justifyContent="flex-end">
+        <View
+          backgroundColor={backgroundColor}
+          borderRadius={20}
+          paddingBottom={100}
+          height={150}
+          width={130}
+        >
+          <View display="flex" justifyContent="center" alignItems="center"> 
+            <Image
+              style={{ width: 100, height: 90, marginTop: -10 }}
+              source={{ uri: getFiles(photo) }}
+            />
+          </View>
+
+          <View marginLeft={10}>
+            <Text marginTop={2} color={"#fff"} fontSize={14} fontWeight={"unset"}>{categoria}</Text>
+            <Text marginTop={5} color={"#fff"} fontWeight="normal">{nome}</Text>
+          </View>
         </View>
       </View>
-    </View>
-  </Pressable>
-);
+    </Pressable>
+  );
+};
